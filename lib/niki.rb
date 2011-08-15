3bbe3fbcfcae7d1fc64f7bd6223df32ec626c0cc
Josep M. Bach <josep.m.bach@gmail.com>
2011-08-15 17:28:49 +0200

    add example

--- lib/niki.rb
+++ lib/niki.rb
@@ -1,4 +1,4 @@
-require 'vendor/archaeopteryx/archaeopteryx'
+require File.expand_path(File.dirname(__FILE__)) + "/../vendor/archaeopteryx/archaeopteryx"
 require 'niki/chords'
 require 'niki/part'
 require 'niki/song'
